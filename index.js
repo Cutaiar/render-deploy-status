d979f4f1eb5874f9619aa71d990380c71030809e
cutaiar <dillonc@vt.edu>
2023-11-13 15:22:54 -0700

    mvp

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 // Tiny node server to make Render deploy status available
-
+require('dotenv').config();
 const express = require('express');
+const api = require('api');
 
 const app = express();
+const renderApi = api('@render-api/v1.0#1ifry11lo91zxhg');
+renderApi.auth(process.env.API_KEY);
+
+const status = (deployStatus) => {
+  // Display strings
+  const unknown = 'Unknown';
+  const inProgress = 'InProgress';
+  const success = 'Success';
+  const failed = 'Failed';
+
+  // If we are given a key other than a string, we don't know what's going on
+  if (typeof deployStatus !== 'string') {
+    return unknown;
+  }
+
+  const statusMap = {
+    created: inProgress,
+    build_in_progress: inProgress,
+    update_in_progress: inProgress,
+    live: success,
+    deactivated: failed,
+    build_failed: failed,
+    update_failed: failed,
+    canceled: failed,
+    pre_deploy_in_progress: inProgress,
+    pre_deploy_failed: failed,
+  };
+  return statusMap[deployStatus] ?? unknown;
+};
 
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, world!'
-  });
+  renderApi.getDeploys({ limit: '1', serviceId: process.env.SERVICE_ID })
+    .then(({ data }) => {
+      res.json({
+        status: status(data[0]?.deploy?.status),
+      });
+    })
+    .catch((err) => {
+      res.json({
+        status: status(null), // Unknown
+      });
+      console.error(err);
+    });
 });
 
 app.listen(3000, () => {
